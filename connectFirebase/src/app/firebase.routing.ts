ee7993e8c6c68cdd1bd3b335b7339c804cf0553f
Lucas Zahir Bukato <lbukato@tgv.com.ar>
2018-07-19 12:12:22 -0300

    asd
    asd

---
+++ connectFirebase/src/app/firebase.routing.ts
@@ -0,0 +1,17 @@
+import { Routes, RouterModule } from '@angular/router';
+import { PerrosFirebaseComponent } from './components/perros-firebase/perros-firebase.component';
+import { PerrosFirebase2Component } from './components/perros-firebase/perros-firebase2.component';
+import { NgModule } from '@angular/core';
+
+
+const APP_ROUTES: Routes = [
+  { path: 'perros', component: PerrosFirebaseComponent},
+  { path: 'perros2', component: PerrosFirebase2Component},
+  { path: '**', pathMatch: 'full', redirectTo: 'body' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(APP_ROUTES)],
+  exports: [RouterModule]
+})
+export class APP_ROUTING { }
